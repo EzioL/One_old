11def28a063b153db2b3fb7110ea44e269f673e5
EzioL <814711679@qq.com>
2016-08-28 20:18:05 +0800

    loading

--- app/src/main/java/com/ezio/one/fragment/HomeFragment.java
+++ app/src/main/java/com/ezio/one/fragment/HomeFragment.java
@@ -1,11 +1,13 @@
 package com.ezio.one.fragment;
 
 import android.app.Fragment;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.ezio.one.R;
 
@@ -23,6 +25,10 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view =inflater.inflate(R.layout.fagment_home,null);
         ButterKnife.bind(view);
+        ImageView image = (ImageView) view.findViewById(R.id.loading);
+        image.setBackgroundResource(R.drawable.loading);
+        AnimationDrawable animation = (AnimationDrawable) image.getBackground();
+        animation.start();
         return view;
     }
 }
